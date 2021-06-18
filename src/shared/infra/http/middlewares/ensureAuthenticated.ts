bfb342bac788179ec2601b1aca4bc7798ef44bcb
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-06-18 14:46:17 -0300

    feat: Create conditional test

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,14 +1,35 @@
 import { NextFunction, Request, Response } from "express";
+import { IncomingHttpHeaders } from "http";
 import { container } from "tsyringe";
 
 import { FirebaseAuthProvider } from "@shared/container/providers/AuthProvider/implementations/FirebaseAuthProvider";
 import { AppError } from "@shared/errors/AppError";
+
+const isTest = process.env.NODE_ENV === "test";
+
+interface IHeadersTest extends IncomingHttpHeaders {
+  firebase_id: string;
+  email: string;
+}
 
 export async function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
+  if (isTest) {
+    const { email, firebase_id } = request.headers as IHeadersTest;
+
+    request.user = {
+      firebase_id,
+      email,
+    };
+
+    next();
+
+    return;
+  }
+
   const authHeader = request.headers.authorization;
   const authTokenProvider = container.resolve(FirebaseAuthProvider);
 
