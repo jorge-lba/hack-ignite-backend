c351d1384d11b7bfc40e62317676a230634c0b13
WelissonLuca <welissonluca17@gmail.com>
2021-06-18 09:50:31 -0300

    feat: add execute condominium use case

--- src/modules/accounts/useCases/createCondominium/CreateCondominiumUseCase.ts
+++ src/modules/accounts/useCases/createCondominium/CreateCondominiumUseCase.ts
@@ -2,10 +2,14 @@ import { inject, injectable } from "tsyringe";
 
 import { ICreateCondominiumDTO } from "@modules/accounts/dtos/ICreateCondominiumDTO";
 import { ICondominiumRepository } from "@modules/accounts/repositories/ICondominiumRepository";
+import { IOperatorsRepository } from "@modules/accounts/repositories/IOperatorsRepository";
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 class CreateCondominiumUseCase {
   constructor(
+    @inject("OperatorsRepository")
+    private operatorsRepository: IOperatorsRepository,
     @inject("CondominiumRepository")
     private condominiumRepository: ICondominiumRepository
   ) {}
@@ -19,7 +23,16 @@ class CreateCondominiumUseCase {
     const condominiumAlreadyExists =
       await this.condominiumRepository.findByEmail(email);
 
-    if (condominiumAlreadyExists) throw new Error("Condominium already exists");
+    if (condominiumAlreadyExists) {
+      throw new AppError("Condominium already exists");
+    }
+    const operatorAlreadyExists = await this.operatorsRepository.findByEmail(
+      email
+    );
+
+    if (operatorAlreadyExists) {
+      throw new AppError("Email is being used!");
+    }
 
     await this.condominiumRepository.create({
       name,
@@ -29,3 +42,5 @@ class CreateCondominiumUseCase {
     });
   }
 }
+
+export { CreateCondominiumUseCase };
