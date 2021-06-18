b298f78db95d9940dee5ae4c9eca93422939851f
WelissonLuca <welissonluca17@gmail.com>
2021-06-18 17:10:57 -0300

    feat: add method patch operator finish register

--- src/shared/infra/http/routes/accounts.routes.ts
+++ src/shared/infra/http/routes/accounts.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateCondominiumController } from "@modules/accounts/useCases/createCondominium/CreateCondominiumController";
 import { CreateOperatorController } from "@modules/accounts/useCases/createOperator/CreateOperatorController";
+import { FinishRegisterOperatorController } from "@modules/accounts/useCases/finishRegisterOperator/FinishRegisterOperatorController";
 
 const accountsRoutes = Router();
 
@@ -9,8 +10,11 @@ const createOperatorController = new CreateOperatorController();
 
 const createCondominiumController = new CreateCondominiumController();
 
+const finishRegisterOperatorController = new FinishRegisterOperatorController();
+
 accountsRoutes.post("/condominiums", createCondominiumController.handle);
 
 accountsRoutes.post("/operators", createOperatorController.handle);
+accountsRoutes.patch("/operators", finishRegisterOperatorController.handle);
 
 export { accountsRoutes };
