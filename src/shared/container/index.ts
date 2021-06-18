b4a2dc40bdec2e30b9d7dba2fd1b1ba62e7c3720
WelissonLuca <welissonluca17@gmail.com>
2021-06-18 02:09:26 -0300

    feat: add singleton container condominium

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,11 @@
 import { container } from "tsyringe";
 
 import "@shared/container/providers";
+
+import { CondominiumRepository } from "@modules/accounts/infra/typeorm/repositories/CondominiumRepository";
+import { ICondominiumRepository } from "@modules/accounts/repositories/ICondominiumRepository";
+
+container.registerSingleton<ICondominiumRepository>(
+  "CondominiumRepository",
+  CondominiumRepository
+);
