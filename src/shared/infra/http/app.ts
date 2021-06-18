09a19ecd25f06fad5c344fd43c222c4b9a51bbe5
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-06-17 22:46:26 -0300

    feat:server-app base

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import "dotenv/config";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(500).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(400).json({
+      status: "error",
+      message: `Interval server error = ${err}`,
+    });
+  }
+);
+
+export { app };
