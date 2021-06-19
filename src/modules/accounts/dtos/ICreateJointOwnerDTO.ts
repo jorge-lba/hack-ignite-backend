3ab01c959aa92c6ef1cfc097ba127e7b6c1b1af9
WelissonLuca <welissonluca17@gmail.com>
2021-06-19 14:46:29 -0300

    feat: add create joint owner DTO

---
+++ src/modules/accounts/dtos/ICreateJointOwnerDTO.ts
@@ -0,0 +1,10 @@
+interface ICreateJointOwnerDTO {
+  name: string;
+  phone: string;
+  road?: string;
+  block?: string;
+  number: number;
+  condominium_id: string;
+}
+
+export { ICreateJointOwnerDTO };
