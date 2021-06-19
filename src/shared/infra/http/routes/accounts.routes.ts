0d97efaeb64aec74b06d001cddbfbd5f43afc724
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-06-18 23:07:01 -0300

    feat:ListbyCondominiumId Route

--- src/shared/infra/http/routes/accounts.routes.ts
+++ src/shared/infra/http/routes/accounts.routes.ts
@@ -2,15 +2,18 @@ import { Router } from "express";
 
 import { CreateCondominiumController } from "@modules/accounts/useCases/createCondominium/CreateCondominiumController";
 import { CreateOperatorController } from "@modules/accounts/useCases/createOperator/CreateOperatorController";
+import { ListOperatorsController } from "@modules/accounts/useCases/listOperators/ListOperatorsController";
 
 const accountsRoutes = Router();
 
 const createOperatorController = new CreateOperatorController();
+const listOperatorsController = new ListOperatorsController();
 
 const createCondominiumController = new CreateCondominiumController();
 
 accountsRoutes.post("/condominiums", createCondominiumController.handle);
 
 accountsRoutes.post("/operators", createOperatorController.handle);
+accountsRoutes.get("/operators", listOperatorsController.handle);
 
 export { accountsRoutes };
