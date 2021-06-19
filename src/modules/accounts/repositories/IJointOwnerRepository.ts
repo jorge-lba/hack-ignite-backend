e88f46805782a1638e1f1238d4db771316e17989
WelissonLuca <welissonluca17@gmail.com>
2021-06-19 14:46:46 -0300

    feat: add joint owner repository

---
+++ src/modules/accounts/repositories/IJointOwnerRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateJointOwnerDTO } from "../dtos/ICreateJointOwnerDTO";
+import { JointOwners } from "../infra/typeorm/entities/JointOwners";
+
+interface IJointOwnerRepository {
+  create(data: ICreateJointOwnerDTO): Promise<void>;
+  listByCondominiumId: (condominium_id: string) => Promise<JointOwners[]>;
+  findById: (id: string) => Promise<JointOwners>;
+  updateById: (id: string, data: ICreateJointOwnerDTO) => Promise<void>;
+  delete(id: string): Promise<void>;
+}
+
+export { IJointOwnerRepository };
