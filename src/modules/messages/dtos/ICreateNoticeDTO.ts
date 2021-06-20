84aafcc1b3b2f4b0b68925bea172da06120bf912
Levy <levyzudo@msn.com>
2021-06-20 15:46:46 -0300

    feat:firestore-create-condominium

--- src/modules/messages/dtos/ICreateNoticeDTO.ts
+++ src/modules/messages/dtos/ICreateNoticeDTO.ts
@@ -1,5 +1,6 @@
 interface ICreateNoticeDTO {
   id: string;
+  user_id: string;
   name: string;
   body: string;
   timestamp: string;
--- src/modules/messages/infra/fireorm/collections/Notices.ts
+++ src/modules/messages/infra/fireorm/collections/Notices.ts
@@ -1,5 +1,6 @@
 export class Notices {
   id: string;
+  user_id: string;
   name: string;
   body: string;
   timestamp: string;
