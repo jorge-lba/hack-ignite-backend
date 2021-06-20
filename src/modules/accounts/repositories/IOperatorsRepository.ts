5427244e6dbad077fc746c419fefb88ee0eee99f
WelissonLuca <welissonluca17@gmail.com>
2021-06-20 14:35:35 -0300

    refactor: add returns in method create operator

--- src/modules/accounts/repositories/IOperatorsRepository.ts
+++ src/modules/accounts/repositories/IOperatorsRepository.ts
@@ -2,7 +2,7 @@ import { ICreateOperatorDTO } from "../dtos/ICreateOperatorDTO";
 import { Operators } from "../infra/typeorm/entities/Operators";
 
 interface IOperatorsRepository {
-  create: ({ email, condominium_id }: ICreateOperatorDTO) => Promise<void>;
+  create: ({ email, condominium_id }: ICreateOperatorDTO) => Promise<Operators>;
   findByEmail: (email: string) => Promise<Operators>;
   findById: (id: string) => Promise<Operators>;
   findOneByFirebaseId(firebase_id: string): Promise<Operators>;
